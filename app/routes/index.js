788c63e300fa750dbeb559d3a9a1e5041e410c30
ctopheryoung@gmail.com <ctopheryoung@gmail.com>
2016-03-28 09:52:13 -0700

    minor code refactor

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,15 +5,15 @@ export default Ember.Route.extend({
     return this.store.findAll('rental');
   },
   actions: {
-  save3(params) {
-    var newRental = this.store.createRecord('rental', params);
-    newRental.save();
-    this.transitionTo('index');
-  },
-  
-  destroyRental(rental) {
-    rental.destroyRecord();
-    this.transitionTo('index');
+    save3(params) {
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
+      this.transitionTo('index');
+    },
+
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
   }
-}
 });
