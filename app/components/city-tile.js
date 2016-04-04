14a91bddd6a9ee99943f8972a2b2b1c1215c1d83
Austin Minnon and Ramon de la Cruz <whi7ee@gmail.com>
2016-04-04 10:07:57 -0700

    add helpers and computed properties to sort with more detail

--- app/components/city-tile.js
+++ app/components/city-tile.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  fullLocation: Ember.computed('city.name', 'city.country', function() {
+    return this.get('city.name') + ', ' + this.get('city.country');
+  }),
+
+  sortBy: ['cost:desc'],
+  sortedRentals: Ember.computed.sort('city.rentals', 'sortBy'),
+
   actions: {
     destroyCity(city) {
       if(confirm('Hey Godzilla, slow down.')) {
